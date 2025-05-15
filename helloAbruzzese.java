dcba5df540eacfe427d7f7d6bb0f5ee426f31d19
Danylarue19126 <elias.fouadaziz@capellinisauro.edu.it>
2025-05-15 06:44:23 +0000

    Modifica dal compagno

--- helloAbruzzese.java
+++ helloAbruzzese.java
@@ -6,5 +6,6 @@ public class helloAbruzzese {
         System.out.println(x:"10/01/2007");//stampo data di nascita
 
         System.out.println(x:"Forza Follo !");//stampo la squadra del cuore
+        System.out.println(x:"elias mio capo!");//stampo il mio capo
     }
 }
